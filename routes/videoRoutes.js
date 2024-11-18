fce088808f5a44ed36f0147b4f27f6ced880f31e
Ghulam Muhammad <95461763+20I-0923@users.noreply.github.com>
2024-11-18 20:56:28 +0500

    Middleware readded for video apis

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -16,19 +16,19 @@ import { authenticateToken } from "../middlewares/authenticateToken.js";
 export const videoRoutes = Router();
 
 //Save a new video
-videoRoutes.post("/saveNewVideo", saveNewVideo);
+videoRoutes.post("/saveNewVideo", authenticateToken,  saveNewVideo);
 
 //Update a video
-videoRoutes.put("/updateVideo", updateVideo);
+videoRoutes.put("/updateVideo", authenticateToken, updateVideo);
 
 //Delete a video
-videoRoutes.delete("/deleteVideo/:accountId/:videoId", deleteVideo);
+videoRoutes.delete("/deleteVideo/:accountId/:videoId", authenticateToken, deleteVideo);
 
 //Get all videos by account id
-videoRoutes.get("/getVideosByAccountId/:id", getVideosByAccountId);
+videoRoutes.get("/getVideosByAccountId/:id", authenticateToken, getVideosByAccountId);
 
 //Get all videos
-videoRoutes.get("/getAllVideos", getAllVideos);
+videoRoutes.get("/getAllVideos", authenticateToken, getAllVideos);
 
 //Get video by id
-videoRoutes.get("/getVideoById/:id", getVideoById);
+videoRoutes.get("/getVideoById/:id", authenticateToken, getVideoById);
